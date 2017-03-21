1cb459d0cc6e7acc239577abcbe455e83dec2ec4
Frosty21 <nathan.froese21@gmail.com>
2017-03-21 02:11:19 -0700

    before action included to only allow logged in users to comment

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :require_login
   def create
     @product = Product.find(params[:product_id])
     @review - @product.reviews.create(review_params)
@@ -10,8 +11,15 @@ class ReviewsController < ApplicationController
       render_to @product
     end
   end
+
   private
   def review_params
     params.require(:review).premit(:rating, :description)
+  end
+
+  def require_login
+    return true if current_user
+    flash[:error] = "Not logged in to post a review..."
+    render 'products/show'
   end
 end
